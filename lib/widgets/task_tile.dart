b425e0959c3629f8fb3e819691b4d2f31161d5b4
Ritwiz <madangopalsingh2803@gmail.com>
2024-06-19 20:13:27 +0530

    changing the checkbox and the text using callback

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_import, prefer_const_constructors, use_key_in_widget_constructors, library_private_types_in_public_api
+// ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api, prefer_const_constructors_in_immutables
 
 import 'package:flutter/material.dart';
 
@@ -8,20 +8,46 @@ class TasksTile extends StatefulWidget {
 }
 
 class _TasksTileState extends State<TasksTile> {
-  bool isChecked = false;
+  bool isChecked = true;
+
+  void checkBoxCallBack(bool? value) {
+    setState(() {
+      isChecked = value ?? false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a Task.'),
-      trailing: Checkbox(
-        value: isChecked,
-        onChanged: (value) {
-          setState(() {
-            isChecked = value ?? false;
-          });
-        },
+      title: Text(
+        'This is a Task.',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
+      trailing: TaskCheckBox(
+        checkBoxState: isChecked,
+        toggleCheckBoxState: checkBoxCallBack,
+      ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkBoxState;
+  final Function(bool?) toggleCheckBoxState;
+
+  TaskCheckBox({
+    required this.checkBoxState,
+    required this.toggleCheckBoxState,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkBoxState,
+      activeColor: Colors.lightBlueAccent,
+      onChanged: toggleCheckBoxState,
     );
   }
 }
